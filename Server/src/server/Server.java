64731506528b33ec2d839bd90382d416ff900e6c
felipelageduarte@gmail.com <felipelageduarte@felipe-pc.(none)>
2012-06-30 17:11:40 -0300

    New Log package

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -4,16 +4,73 @@
  */
 package server;
 
-/**
- *
- * @author felipelageduarte
- */
-public class Server {
+import Log.Log;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Server implements Runnable {
+
+    protected int serverPort = 8080;
+    protected ServerSocket serverSocket = null;
+    protected boolean isStopped = false;
+    protected Thread runningThread = null;
+
+    public Server(int port) {
+        this.serverPort = port;
+    }
+
+    public synchronized void stop() {
+        this.isStopped = true;
+        try {
+            this.serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
+        }
+    }
+
+    private void openServerSocket() throws IOException {
+        this.serverSocket = new ServerSocket(this.serverPort);
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            this.runningThread = Thread.currentThread();
+        }
+        
+        //try to open server socket
+        try {
+            openServerSocket();
+        } catch (IOException ex) {
+            Log.error("Error closing server: " + ex.getMessage());
+        }
+        while (!isStopped()) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException e) {
+                if (isStopped()) {
+                    System.out.println("Server Stopped.");
+                    return;
+                }
+                throw new RuntimeException(
+                        "Error accepting client connection", e);
+            }
+            new Thread(
+                    new WorkerRunnable(
+                    clientSocket, "Multithreaded Server")).start();
+        }
+        System.out.println("Server Stopped.");
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Log.debug("!!!debug!!!");
+        Log.error("!!!error!!!");
+        Log.fatal("!!!fatal!!!");
+        Log.info("!!!info!!!");
+        Log.warn("!!!warn!!!");
+        Log.warn("!!!warn2!!!");
     }
 }
